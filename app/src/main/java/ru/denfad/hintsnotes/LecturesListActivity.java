475a9f0aecec6042a5d9c5886fe85a5270473635
denfad <denfad2003@mail.ru>
2020-04-03 16:32:06 +0300

    Улучшен UI

--- app/src/main/java/ru/denfad/hintsnotes/LecturesListActivity.java
+++ app/src/main/java/ru/denfad/hintsnotes/LecturesListActivity.java
@@ -1,5 +1,6 @@
 package ru.denfad.hintsnotes;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,9 +9,11 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
@@ -39,7 +42,7 @@ import ru.denfad.hintsnotes.models.Hint;
 
 public class LecturesListActivity extends AppCompatActivity {
     public ListView listView;
-    public SharedPreferences sharedPreferences;
+    public SharedPreferences sharedPreferencesLectures;
     public List<String> lectures = new ArrayList<>();
     public ImageButton addLecture;
     public ArrayAdapter adapter;
@@ -51,19 +54,23 @@ public class LecturesListActivity extends AppCompatActivity {
         listView= findViewById(R.id.lecture_list);
         addLecture=findViewById(R.id.addLecture);
 
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(LecturesListActivity.this);
+
+
+        sharedPreferencesLectures = PreferenceManager.getDefaultSharedPreferences(LecturesListActivity.this);
         GsonBuilder gsonBuilder = new GsonBuilder();
 
 
-        if(!sharedPreferences.getString("lectures","[]").equals("[]")){
-            Log.i("Get lectures", sharedPreferences.getString("lectures","[]"));
-            lectures.addAll(Arrays.asList(gsonBuilder.create().fromJson(sharedPreferences.getString("lectures","[]"), String[].class)));
+        if(!sharedPreferencesLectures.getString("lectures","[]").equals("[]")){
+            Log.i("Get lectures", sharedPreferencesLectures.getString("lectures","[]"));
+            lectures.addAll(Arrays.asList(gsonBuilder.create().fromJson(sharedPreferencesLectures.getString("lectures","[]"), String[].class)));
         }
 
         //создание адаптера
-        adapter = new ArrayAdapter<String>(
-                this, R.layout.lecture,lectures);
+
+        adapter = new LectureAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,lectures);
         listView.setAdapter(adapter);
+
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -102,7 +109,7 @@ public class LecturesListActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         lectures.remove(lectureName);
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        SharedPreferences.Editor editor = sharedPreferencesLectures.edit();
                         GsonBuilder gsonBuilder = new GsonBuilder();
                         editor.putString("lectures", gsonBuilder.create().toJson(lectures));
                         adapter.notifyDataSetChanged();
@@ -139,7 +146,7 @@ public class LecturesListActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         lectures.add(input.getText().toString());
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        SharedPreferences.Editor editor = sharedPreferencesLectures.edit();
                         GsonBuilder gsonBuilder = new GsonBuilder();
                         editor.putString("lectures", gsonBuilder.create().toJson(lectures));
                     }
@@ -155,7 +162,7 @@ public class LecturesListActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         GsonBuilder gsonBuilder = new GsonBuilder();
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        SharedPreferences.Editor editor = sharedPreferencesLectures.edit();
         editor.putString("lectures",gsonBuilder.create().toJson(lectures));
         Log.i("Save lectures", gsonBuilder.create().toJson(lectures));
         editor.apply();
@@ -166,7 +173,7 @@ public class LecturesListActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         GsonBuilder gsonBuilder = new GsonBuilder();
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        SharedPreferences.Editor editor = sharedPreferencesLectures.edit();
         editor.putString("lectures", gsonBuilder.create().toJson(lectures));
         Log.i("Save lectures", gsonBuilder.create().toJson(lectures));
         editor.apply();
@@ -176,5 +183,44 @@ public class LecturesListActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.finish();
+    }
+
+    public class LectureAdapter extends ArrayAdapter<String>{
+
+        public LectureAdapter(@NonNull Context context, int resource,@NonNull List<String> objects) {
+            super(context, resource,objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull final ViewGroup parent) {
+            final String lecture = getItem(position);
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.lecture, null);
+
+            LectureHolder holder = new LectureHolder();
+            holder.title = convertView.findViewById(R.id.text1);
+            holder.hintsNumber = convertView.findViewById(R.id.hintsNumber);
+
+            holder.title.setText(lecture);
+
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            if(!sharedPreferencesLectures.getString(lecture,"[]").equals("[]")) {
+                List<Hint> hints = Arrays.asList(gsonBuilder.create().fromJson(sharedPreferencesLectures.getString(lecture, "[]"), Hint[].class));
+                holder.hintsNumber.setText("Заметок: "+hints.size());
+            }
+            else holder.hintsNumber.setText("Нет заметок");
+
+            convertView.setTag(holder);
+
+            return convertView;
+        }
+    }
+
+    //внутренний класс фрагмента заметки для инициализации его объектов (кнопок и полей)
+    private static  class LectureHolder {
+        public TextView title, hintsNumber;
+
+
     }
 }
